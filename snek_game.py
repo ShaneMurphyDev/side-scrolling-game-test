a16a1cd77a0fea9a13f5a167a0ef404a4a564f04
Shane Murphy <141724797+ShaneMurphyDev@users.noreply.github.com>
2025-04-05 13:18:27 +0000

    deleted py file copy

---
+++ snek_game.py
@@ -0,0 +1,8 @@
+import turtle
+
+# Screen set up
+wn = turtle.Screen()
+wn.title("Snek Game")
+wn.bgcolor("white")
+wn.setup(width=600, height=600)
+wn.tracer(0)
--- snek_game1.py
+++
@@ -1,8 +0,0 @@
-import turtle
-
-# Screen set up
-wn = turtle.Screen()
-wn.title("Snek Game")
-wn.bgcolor("white")
-wn.setup(width=600, height=600)
-wn.tracer(0)
